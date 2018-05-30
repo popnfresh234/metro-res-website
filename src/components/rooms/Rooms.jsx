c1d7b6cd72ea7fb61e8fdf81c6bbbd41afe8e882
Alex <ajhollid@gmail.com>
2018-05-30 16:41:36 -0700

    Implement click handling on image links

--- src/components/rooms/Rooms.jsx
+++ src/components/rooms/Rooms.jsx
@@ -3,8 +3,21 @@ import { Container, Segment, Grid, Image, GridRow, GridColumn } from 'semantic-u
 import ReactRevealText from 'react-reveal-text';
 import { Link } from 'react-router-dom';
 
-
 class Rooms extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      room: 'rooma',
+    };
+    this.handleImageClick = this.handleImageClick.bind( this );
+  }
+
+  handleImageClick( room, e ) {
+    e.preventDefault();
+    this.setState( {
+      room,
+    } );
+  }
   render() {
     return (
       <Grid.Row>
@@ -22,33 +35,33 @@ class Rooms extends Component {
             <Grid stackable>
               <GridRow>
                 <GridColumn width={8}>
-                  <Image src="src/images/rooms/rooma.jpg" />
+                  <Image as="a" href="" src="src/images/rooms/rooma.jpg" onClick={e => this.handleImageClick( 'rooma', e )} />
                 </GridColumn>
                 <GridColumn width={8}>
-                  <Image src="src/images/rooms/roomb.jpg" />
-                </GridColumn>
-              </GridRow>
-              <GridRow>
-                <GridColumn width={8}>
-                  <Image src="src/images/rooms/roomc.jpg" />
-                </GridColumn>
-                <GridColumn width={8}>
-                  <Image src="src/images/rooms/roomd.jpg" />
+                  <Image as="a" href="" src="src/images/rooms/roomb.jpg" onClick={e => this.handleImageClick( 'roomb', e )} />
                 </GridColumn>
               </GridRow>
               <GridRow>
                 <GridColumn width={8}>
-                  <Image src="src/images/rooms/roome.jpg" />
+                  <Image as="a" href="" src="src/images/rooms/roomc.jpg" onClick={e => this.handleImageClick( 'roomc', e )} />
                 </GridColumn>
                 <GridColumn width={8}>
-                  <Image src="src/images/rooms/roomf.jpg" />
+                  <Image as="a" href="" src="src/images/rooms/roomd.jpg" onClick={e => this.handleImageClick( 'roomd', e )} />
+                </GridColumn>
+              </GridRow>
+              <GridRow>
+                <GridColumn width={8}>
+                  <Image as="a" href="" src="src/images/rooms/roome.jpg" onClick={e => this.handleImageClick( 'roome', e )} />
+                </GridColumn>
+                <GridColumn width={8}>
+                  <Image as="a" href="" src="src/images/rooms/roomf.jpg" onClick={e => this.handleImageClick( 'roomf', e )} />
                 </GridColumn>
               </GridRow>
             </Grid>
           </Segment>
         </Grid.Column>
         <Grid.Column width={13}>
-          <Image src="src/images/rooms/rooma.jpg" />
+          <Image src={`src/images/rooms/${this.state.room}.jpg`} />
         </Grid.Column>
       </Grid.Row>
     );
