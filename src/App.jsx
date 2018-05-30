963ba2bd06591ee6e2850c21e9927877c6a375d8
Alex <ajhollid@gmail.com>
2018-05-30 15:10:42 -0700

    Add nav-bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from '../src/components/home/Home.jsx';
-import {
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
+import NavBar from '../src/components/nav-bar/NavBar.jsx';
+
 
 class App extends Component {
   render() {
     return (
 
       <div className="App">
+        <NavBar />
         <Grid stackable>
           <Switch>
             <Route path="/" exact component={Home} />
---
+++ src/components/nav-bar/NavBar.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Menu, Grid } from 'semantic-ui-react';
+import ReactRevealText from 'react-reveal-text';
+import { Link } from 'react-router-dom';
+
+
+class Home extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      activeItem: 'home',
+    };
+    this.handleItemClick = this.handleItemClick.bind( this );
+  }
+
+  handleItemClick( e, { name } ) {
+    this.setState( {
+      activeItem: name,
+    } );
+  }
+
+  render() {
+    const { activeItem } = this.state;
+    return (
+      <Menu pointing secondary>
+        <Menu.Item name="home" active={activeItem === 'home'} onClick={this.handleItemClick} />
+        <Menu.Item name="rooms" active={activeItem === 'rooms'} onClick={this.handleItemClick} />
+        <Menu.Item name="facilities" active={activeItem === 'facilities'} onClick={this.handleItemClick} />
+        <Menu.Item name="location" active={activeItem === 'location'} onClick={this.handleItemClick} />
+        <Menu.Item name="gallery" active={activeItem === 'gallery'} onClick={this.handleItemClick} />
+        <Menu.Item name="contact" active={activeItem === 'contact'} onClick={this.handleItemClick} />
+      </Menu>
+    );
+  }
+}
+
+export default Home;
