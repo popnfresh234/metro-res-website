3c65b39a733c832cc58a730668269f7abad0bd02
Alex <ajhollid@gmail.com>
2018-05-30 22:00:31 -0700

    Add menu item

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { Grid, Menu, Item } from 'semantic-ui-react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
 
 
 class NavMenu extends Component {
+  constructor( props ) {
+    super( props );
+    this.handleItemClick = this.handleItemClick.bind( this );
+  }
+
+  handleItemClick( name, e ) {
+    e.preventDefault();
+    this.setState( {
+      activeItem: name,
+    } );
+  }
   render() {
+    const activeItem = this.state;
     return (
-      <Menu className="left-menu" inverted color="blue" fixed="left" style={{ width: '250px' }}>
-        <Item>
-          <p>test</p>
-        </Item>
+      <Menu className="left-menu" widths={1} inverted color="blue" fixed="left" style={{ width: '250px' }}>
+        <Menu.Item
+
+          name="editorials"
+          active={activeItem === 'editorials'}
+          onClick={this.handleItemClick}
+        />
       </Menu>
     );
   }
