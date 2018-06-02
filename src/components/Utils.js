f68afa6d6726f3c1e30d41278f18544a4e87e4b9
Alex <ajhollid@gmail.com>
2018-06-02 08:47:35 -0700

    Fix typo on thumbnail name

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -3,7 +3,7 @@ const ROOM_THUMBS = ['thumba', 'thumbb', 'thumbc', 'thumbd', 'thumbe', 'thumbf']
 const FACILITY_IMAGES = ['kitchena', 'livinga', 'kitchenb', 'batha', 'bathb', 'bathc', 'halla', 'kitchene'];
 const FACILITY_THUMBS = ['thumbkitchena', 'thumblivinga', 'thumbkitchenb', 'thumbbatha', 'thumbbathb', 'thumbbathc', 'thumbhalla', 'thumbkitchene'];
 const GALLERY_IMAGES = ['gallerya', 'galleryb', 'galleryc', 'galleryd', 'gallerye', 'galleryf', 'galleryg'];
-const GALLERY_THUMBS = ['thumbgallerya', 'thumbgalleryb', 'thumbgalleryc', 'vgalleryd', 'thumbgallerye', 'thumbgalleryf', 'thumbgalleryg'];
+const GALLERY_THUMBS = ['thumbgallerya', 'thumbgalleryb', 'thumbgalleryc', 'thumbgalleryd', 'thumbgallerye', 'thumbgalleryf', 'thumbgalleryg'];
 
 const ROOM_CAPTION = 'Metro Residenecs offers a variety of newly renovated, fully furnished rooms including laminate flooring, fridge, desk and task chair, lamp, and storage. All rooms have large, screened windows offering light and fresh air.';
 const FACILITY_CAPTION = 'Welcome to our shared community; including a bright airy kitchen, washer/dryer, comfortable common room where you can enjoy a new 40 inch LCD TV, cable, and wireless internet. Large outside deck is a fun spot to play ping pong, have picnics, or just read in the sun. The back yard is a large space in which gardeners can grow their favourite plants';
