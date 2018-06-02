0cd648e47db9f018f1f144164b54fe1b13ae5dc5
Alex <ajhollid@gmail.com>
2018-06-02 09:49:34 -0700

    Redesign Home page with Card

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,48 +5,50 @@ class Contact extends Component {
   render() {
     return (
       <div className="container-div" >
-        <Segment compact raised>
-          <Card fluid>
+
+        <Card fluid style={{ maxWidth: '700px' }}>
+          <Segment compact raised>
             <Image src="/src/images/gallery/gallerya.jpg" />
-            <Card.Content>
-              <Card.Header>
+          </Segment>
+          <Card.Content>
+            <Card.Header>
                 Metro Residences
-              </Card.Header>
-              <Card.Meta>
+            </Card.Header>
+            <Card.Meta>
                 Rooms are available from 500-600 per month
-              </Card.Meta>
-              <br />
-              <Card.Description>
-                <Header as="h5">
+            </Card.Meta>
+            <br />
+            <Card.Description>
+              <Header as="h5">
               Address
-                </Header>
+              </Header>
                 531 East Georgia St.
                 V6A-1Z8
                 Vancouver, BC, Canada
-              </Card.Description>
-              <br />
-              <Card.Description>
-                <Header as="h5">
+            </Card.Description>
+            <br />
+            <Card.Description>
+              <Header as="h5">
               Phone
-                </Header>
+              </Header>
                 604 722 3413
-              </Card.Description>
-              <br />
-              <Card.Description>
-                <Header as="h5" style={{ marginBottom: '0px' }}>
-              Email
-                </Header>
-                <a href="mailto:booking@metro-res.com">booking@metro-res.com</a>
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <a>
-                <Icon name="user" />
-        22 Friends
-              </a>
-            </Card.Content>
-          </Card>
-        </Segment>
+            </Card.Description>
+            <br />
+            <Card.Description>
+              <Header as="h5" style={{ marginBottom: '0px' }}>
+                  Email
+              </Header>
+              <a href="mailto:booking@metro-res.com">booking@metro-res.com</a>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="user" />
+                  22 Friends
+            </a>
+          </Card.Content>
+        </Card>
+
       </div>
     );
   }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,41 @@
 import React, { Component } from 'react';
-import { Container, Segment, Image, Header } from 'semantic-ui-react';
+import { Container, Card, Segment, Image, Header, Icon } from 'semantic-ui-react';
 
 class Home extends Component {
   render() {
     return (
-      <div className="container-div">
-        <Segment raised compact >
-          <Image src="src/images/vancouver.jpg" />
-        </Segment>
-        <Segment raised compact>
-          <Container textAlign="left">
-            <Header as="h1">
-            Welcome to Metro Residences, located in the heart of beautiful Vancouver.
-            </Header>
+      <div className="container-div" >
 
-            <Header as="h5">
-            Metro Residences offers the opportunity
+        <Card fluid style={{ maxWidth: '700px' }}>
+          <Segment compact raised>
+            <Image src="src/images/vancouver.jpg" />
+          </Segment>
+          <Card.Content>
+            <Card.Header>
+              Welcome to Metro Residences, located in the heart of beautiful Vancouver
+            </Card.Header>
+            <br />
+            <Card.Description>
+              <Header as="h5">
+                Metro Residences offers the opportunity
             to live in and experience this world class city at an excellent value.
-            </Header>
-            <Header as="h5">
+              </Header>
+
+              <Header as="h5">
             Metro Residences can be your safe, secure home away from home, whether
             working or studying. We provide a peaceful, quiet room of your own to
             anybody on a budget needing tidy and comfortable surroundings
-            </Header>
-          </Container>
-        </Segment>
+              </Header>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <i className="far fa-envelope" />
+              <a href="mailto:booking@metro-res.com">  booking@metro-res.com</a>
+            </a>
+          </Card.Content>
+        </Card>
+
       </div>
     );
   }
